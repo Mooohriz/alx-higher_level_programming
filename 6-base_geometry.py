e991d701f327a95704f058189733abc9d29db9ad
Mohammad Yekini <105173976+Mooohriz@users.noreply.github.com>
2022-09-26 23:51:57 +0100

    yes6

--- 6-base_geometry.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines an BaseGeometry class."""
-
-
-class BaseGeometry:
-    """Add a public instance method."""
-    def area(self):
-        raise Exception("area() is not implemented")
